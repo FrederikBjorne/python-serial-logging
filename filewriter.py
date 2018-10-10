4f3de2a8daa7812f11a4387f419f4a4390c86942
Spaceman <frederik.bjorne@gmail.com>
2018-10-10 10:07:25 +0200

    Make main program work for real serial port and wait for user
    The program slept for 1 second and exit program which is bad for
    reading from a real serial port. Here the program is stopped by
    user only. A callback has been added to reader- and writer threads
    so they can report that they have failed and are terminated.

--- filewriter.py
+++ filewriter.py
@@ -9,8 +9,8 @@ import codecs
 
 class FileWriter(Thread):
     '''
-    This thread reads text from a queue and writes these to a file passed as log_file_path.
-    The text queue is filled with new text lines by calling put().
+    This thread reads log lines from a queue and writes these to a file passed as log_file_path.
+    The log line queue is filled with new log lines by calling put().
     Quits if stop() is called. Setting the read_queue_timer for reading the queue determine
     the responsiveness to stop call and is optional.
     '''
@@ -19,53 +19,69 @@ class FileWriter(Thread):
 
     def __init__(self,
                  log_file_path,
-                 read_queue_timer=READ_NEW_LOGLINE_TMO,
+                 callback,
+                 read_queue_timeout=READ_NEW_LOGLINE_TMO,
                  name=THREAD_NAME,
                  encoding='utf8'):
         super(FileWriter, self).__init__(name = name)
-        self._read_queue_timer = read_queue_timer
+        self._read_queue_timeout = read_queue_timeout
         self._log_file_path = log_file_path
         self._encoding = encoding
 
         self.setDaemon(True)
-        self._text_queue = Queue()
+        self._log_line_queue = Queue()
         self._stop = Event()
         codecs.register_error('backslashreplace', self.backslash_replace)
         self.logger = logging.getLogger(self.THREAD_NAME)
+        self._callback = callback
+
+    def __repr__(self):
+        return '{}({!r}, {!r}, {!r}, {!r})'.format(self.__class__.__name__,
+                                                   self.getName(),
+                                                   self._read_queue_timeout,
+                                                   self._log_file_path,
+                                                   self._encoding)
 
     def put(self, text_line):
         '''
         Puts a text line to the text queue to be written to the specified file for logging.
         :param text_line: A text line to be written to file.
         '''
-        self._text_queue.put(text_line)  # Queue calls are thread-safe
+        self._log_line_queue.put(text_line)  # Queue calls are thread-safe
 
     def stop(self):
         '''
         Stop writing to a log file from the internal queue and commit suicide.
         '''
         self._stop.set()
-        self.logger.debug('stop writer')
-        self.join()
+        self.logger.debug('writer stopped')
+        if self.is_alive():
+            self.join()
+        self.logger.debug('writer has terminated')
 
     @staticmethod
     def backslash_replace(error):
         """
-        An error handler to be called when escape characters are read from the text queue input.
+        An error handler to be called when escape characters are read from the log line queue input.
         """
         return u"".join([u"\\x{:x}".format(ord(error.object[i]))
                          for i in range(error.start, error.end)]), error.end
 
     def run(self):
-        with codecs.open(self._log_file_path, 'wb', self._encoding) as log_file:
-            self.logger.info('Start writing to file.')
-            while not self._stop.is_set():
-                try:  # timeout avoids blocking in order to be responsive to stop calls
-                    log_line = self._text_queue.get(timeout=self._read_queue_timer)
-                except Empty:
-                    continue
-                else:
-                    self._text_queue.task_done()
-                    log_file.write(str(log_line) + '\n')
+        try:
+            with codecs.open(self._log_file_path, 'wb', self._encoding) as log_file:
+                self.logger.info('start writing to file.')
+
+                while not self._stop.is_set():
+                    try:  # timeout avoids blocking in order to be responsive to stop calls
+                        log_line = self._log_line_queue.get(timeout=self._read_queue_timeout)
+                    except Empty:
+                        continue
+                    else:
+                        self._log_line_queue.task_done()
+                        log_file.write(str(log_line) + '\n')
+        except Exception as e:  # this may occur if codecs fails somehow
+            self.logger.error('Error: {}'.format(e))
+            self._callback('{} has stopped running. error: {}'.format(self.getName(), str(e)))  # call back error
 
         self.logger.info('stopped writing to file.')
